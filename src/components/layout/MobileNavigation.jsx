4828a2d644db1d6e83e81ca514190f877bc2a565
Grégory Lama <gregory.lama@gmail.com>
2023-12-17 20:18:19 +0100

    feat(portfolio): fix mobile navigation

--- src/components/layout/MobileNavigation.jsx
+++ src/components/layout/MobileNavigation.jsx
@@ -1,37 +1,25 @@
 import { Link as ScrollLink } from "react-scroll";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { RiCloseLine } from "react-icons/ri";
 
 const MobileNavigation = ({ changeState }) => {
   const router = useRouter();
 
-  const checkroute =
-    router.route !== "/homepage2" && router.route !== "/homepage3";
-
   const handleClick = () => {
     changeState(false);
   };
 
-  return <>
-    <button
-      className="btn btn-small btn-transparent absolute left-auto right-4 top-4 z-10 h-10 w-10 rounded-full p-0 text-center text-3xl"
-      onClick={() => changeState(false)}
-    >
-      <RiCloseLine className="inline" />
-    </button>
-    <nav className="relative max-h-full w-full overflow-y-auto">
-      <ul className="mb-0 list-none pl-0">
-        <li className="block">
-          {checkroute ? (
-            (<Link
-              href="/homepage2"
-              className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
-              onClick={() => handleClick()}>
-              Home<span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
-
-            </Link>)
-          ) : (
+  return (
+    <>
+      <button
+        className="btn btn-small btn-transparent absolute left-auto right-4 top-4 z-10 h-10 w-10 rounded-full p-0 text-center text-3xl"
+        onClick={() => changeState(false)}
+      >
+        <RiCloseLine className="inline" />
+      </button>
+      <nav className="relative max-h-full w-full overflow-y-auto">
+        <ul className="mb-0 list-none pl-0">
+          <li className="block">
             <ScrollLink
               activeClass="text-primary"
               to="section-home"
@@ -43,20 +31,10 @@ const MobileNavigation = ({ changeState }) => {
               onClick={() => handleClick()}
             >
               Home
-              <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
+              <span className="absolute bottom-5 left-0 top-auto inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
             </ScrollLink>
-          )}
-        </li>
-        <li className="block">
-          {checkroute ? (
-            (<Link
-              href="/homepage2"
-              className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
-              onClick={() => handleClick()}>
-              About<span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
-
-            </Link>)
-          ) : (
+          </li>
+          <li className="block">
             <ScrollLink
               activeClass="text-primary"
               to="section-about"
@@ -68,20 +46,10 @@ const MobileNavigation = ({ changeState }) => {
               onClick={() => handleClick()}
             >
               About
-              <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
+              <span className="absolute bottom-5 left-0 top-auto inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
             </ScrollLink>
-          )}
-        </li>
-        <li className="block">
-          {checkroute ? (
-            (<Link
-              href="/homepage2"
-              className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
-              onClick={() => handleClick()}>
-              Resume<span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
-
-            </Link>)
-          ) : (
+          </li>
+          <li className="block">
             <ScrollLink
               activeClass="text-primary"
               to="section-resume"
@@ -93,20 +61,10 @@ const MobileNavigation = ({ changeState }) => {
               onClick={() => handleClick()}
             >
               Resume
-              <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
+              <span className="absolute bottom-5 left-0 top-auto inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
             </ScrollLink>
-          )}
-        </li>
-        <li className="block">
-          {checkroute ? (
-            (<Link
-              href="/homepage2"
-              className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
-              onClick={() => handleClick()}>
-              Works<span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
-
-            </Link>)
-          ) : (
+          </li>
+          <li className="block">
             <ScrollLink
               activeClass="text-primary"
               to="section-portfolios"
@@ -118,29 +76,10 @@ const MobileNavigation = ({ changeState }) => {
               onClick={() => handleClick()}
             >
               Works
-              <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
+              <span className="absolute bottom-5 left-0 top-auto inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
             </ScrollLink>
-          )}
-        </li>
-        <li className="block">
-          <Link
-            href="/posts/1"
-            className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
-            onClick={() => handleClick()}>
-            Blog<span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
-
-          </Link>
-        </li>
-        <li className="block">
-          {checkroute ? (
-            (<Link
-              href="/homepage2"
-              className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
-              onClick={() => handleClick()}>
-              Contact<span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
-
-            </Link>)
-          ) : (
+          </li>
+          <li className="block">
             <ScrollLink
               activeClass="text-primary"
               to="section-contact"
@@ -152,27 +91,27 @@ const MobileNavigation = ({ changeState }) => {
               onClick={() => handleClick()}
             >
               Contact
-              <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
+              <span className="absolute bottom-5 left-0 top-auto inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
             </ScrollLink>
-          )}
-        </li>
-      </ul>
-      <div className="header-button pt-8">
-        <ScrollLink
-          activeClass="active"
-          to="section-contact"
-          spy={true}
-          smooth="easeInQuad"
-          offset={-74}
-          duration={1000}
-          className="btn btn-small"
-          onClick={() => handleClick()}
-        >
-          <span>Hire Me</span>
-        </ScrollLink>
-      </div>
-    </nav>
-  </>;
+          </li>
+        </ul>
+        <div className="header-button pt-8">
+          <ScrollLink
+            activeClass="active"
+            to="section-contact"
+            spy={true}
+            smooth="easeInQuad"
+            offset={-74}
+            duration={1000}
+            className="btn btn-small"
+            onClick={() => handleClick()}
+          >
+            <span>Hire Me</span>
+          </ScrollLink>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default MobileNavigation;
